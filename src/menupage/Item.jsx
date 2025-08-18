ade742f552d2b08d7bd8697ddd7e77b5d224ab03
toufique717 <toufiquehossain716.com>
2025-08-18 20:47:33 +0600

    65.2

--- src/menupage/Item.jsx
+++ src/menupage/Item.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
 import { Helmet } from 'react-helmet-async';
+import Backcover from '../shared/Backcover';
+import Popularitem from '../pages/PopularItem'
+import backimage from '../assets/backimage.jpg'
 
 const Item = () => {
+    
     return (
         <div>
               <Helmet>
                  <title>Medicine Menu</title>
              </Helmet>
             <h1>This is menu page </h1>
+
+            <Backcover
+            image={backimage}
+            title1={"all medicine"}
+            title2={"welcome our shop"}
+            >
+           </Backcover>
+
+           <Popularitem></Popularitem>
+
+            
         </div>
     );
 };
---
+++ src/shared/Backcover.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Parallax, Background } from 'react-parallax';
+
+const Backcover = ({image, title1,title2}) => {
+    return (
+
+        <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={image}
+        bgImageAlt="the dog"
+        strength={-200}
+    >
+
+
+
+        <div>
+
+
+            <div>
+
+
+            <div className="hero  h-[500px] " >
+          <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md bg-black  bg-opacity-50  px-12 py-10">
+      <h1 className="mb-5 text-5xl uppercase font-bold">{title1}</h1>
+      <p className="mb-5">{title2}</p>
+      
+    </div>
+  </div>
+</div>
+            
+        </div>
+            
+        </div>
+        
+    </Parallax>
+         
+    );
+};
+
+export default Backcover;
